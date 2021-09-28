6d24a5ee4dcdd4a3a560677964e49357ffd964dd
Justin Davis <justinndavis@gmail.com>
2021-09-28 12:13:43 +0100

    sno updates

--- env.sh
+++ env.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-#export IP=192.168.0.91
-#export IP=ocp.datr.eu
-#export IP=master.aws.datr.eu
+
 export IP=api.sno.openshiftlabs.net:6443
-#export IP=api.crc.testing:6443
-#export IP=api.shared-rhpds.rhpds.openshift.opentlc.com:6443
 export USER=justin
-#export USER=jusdavis-redhat.com
 
 export ORG=amazin
 export DEV_PROJECT=${ORG}-dev
@@ -16,14 +11,9 @@ export CICD_PROJECT=cicd
 
 export CURL="curl -k -v"
 export JENKINS_USER=justin-admin-edit-view
-export JENKINS_TOKEN=11b9158e58398d82922e9f2377650185ef
+export JENKINS_TOKEN=11f4274ec59be12eb227a08b08ee13d54b
 export JENKINS=jenkins-cicd.apps.sno.openshiftlabs.net
-#export JENKINS=jenkins-cicd.apps-crc.testing
-#export JENKINS=jenkins-cicd.apps.${IP}
 
-
-#export REGISTRY=nexus3-docker-cicd.apps-crc.testing
-# export REGISTRY=nexus3-cicd.apps.ocp4.openshiftlabs.net
 export QUAYIO_REGISTRY=quay.io
 
 export DOMAIN=$CICD_PROJECT
