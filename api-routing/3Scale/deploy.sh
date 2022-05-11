2e0213f8b70703aeff897b5f5949e5d95d879f6a
Justin Davis <justinndavis@gmail.com>
2022-05-11 14:32:48 +0100

    RHSSO/3Scale Integrarion stuff

--- api-routing/3Scale/deploy.sh
+++ api-routing/3Scale/deploy.sh
@@ -7,7 +7,13 @@ oc project 3scale
 oc delete DeveloperAccount jdcorp-dev-acc-1
 oc delete DeveloperUser justin
 oc delete secret justin-creds-secret
+oc delete product amazin-api-product 
+oc delete backend amazin-api-inventory-backend
+oc delete OpenAPI product-openapi
+
+sleep 30
 
 oc create secret generic justin-creds-secret --from-literal=password=password
 oc apply -f user.yaml
 oc apply -f account.yaml
+oc apply -f open-api.yaml
